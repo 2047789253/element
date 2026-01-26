44ff28e38eebfcc4257e8f8736f01ff21cdfdd1f
Hai <sunbaoqi08@gmail.com>
2026-01-26 21:05:10 +0800

    完善Collapse的功能

--- src/components/Collapse/Collapse.vue
+++ src/components/Collapse/Collapse.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import type { NameType } from './types'
+import { ref } from 'vue'
 defineOptions({
   name: 'VkCollapse',
 })
+const activeNames = ref<NameType[]>([])
+const handleItemClick = (item: NameType) => {
+  const index = activeNames.value.indexOf(item)
+  if (index > -1) {
+    activeNames.value.splice(index, 1)
+  } else {
+    activeNames.value.push(item)
+  }
+}
 </script>
 
 <template>
--- src/components/Collapse/types.ts
+++ src/components/Collapse/types.ts
@@ -1,5 +1,12 @@
+import type { Ref } from 'vue'
+export type NameType = string | number
+
 export interface CollapseProps {
   name?: string | number
   title?: string
   disabled?: boolean
+}
+
+export interface CollapseContext {
+  isItemDisabled: (name: NameType) => boolean
 }
